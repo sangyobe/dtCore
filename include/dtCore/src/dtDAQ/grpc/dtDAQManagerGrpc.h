36a89b89d15c55c7e5787120d640f06ad3ce226b
Sangyup Yi <sean.yi@hyundai.com>
2023-09-26 19:56:53 +0900

    on implementing grpc server.

--- include/dtCore/src/dtDAQ/grpc/dtDAQManagerGrpc.h
+++ include/dtCore/src/dtDAQ/grpc/dtDAQManagerGrpc.h
@@ -31,10 +31,22 @@ public:
     void Initialize() {
         grpc::EnableDefaultHealthCheckService(true);
         grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+        grpc::ServerBuilder builder;
+        builder.AddListeningPort(_server_address, grpc::InsecureServerCredentials());
+        builder.RegisterService(_service.get());
+        _server = builder.BuildAndStart();
+        //_server->Wait();
     }
     void Terminate() {
-        
+        _server->Shutdown();
     }
+    void RegServiceImple(std::unique_ptr<grpc::Service> service) {
+        _service = std::move(service);
+    }
+private:
+    std::unique_ptr<grpc::Server> _server;
+    std::unique_ptr<grpc::Service> _service;
+    std::string _server_address {"0.0.0.0:50051"};
 };
 
 }
